23e0ce8a7db562987ee30d5c258c0dc18569998a
Christina Giancola <christinamgiancola@gmail.com>
2022-02-10 10:32:12 -0500

    WIP mapping links to responsive bgImage, made LinkGrid component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import LinkGrid from "../components/linkGrid"
 
 import "normalize.css"
 import "../assets/main.css"
@@ -12,23 +13,7 @@ export default function Home() {
         className={lightsOn ? "bgImage lightsOn" : "bgImage lightsOff"}
       ></div>
       <div className="linkContainer" onClick={() => setLightsOn(!lightsOn)}>
-        <div className="linkGrid">
-          <div className="link linkA"></div>
-          <div className="link linkB"></div>
-          <div className="link linkC"></div>
-          <div className="link linkD"></div>
-          <div className="link linkE"></div>
-          <div className="link linkF"></div>
-          <div className="link linkG"></div>
-          <div className="link linkH"></div>
-          <div className="link linkI"></div>
-          <div className="link linkJ"></div>
-          <div className="link linkK"></div>
-          <div className="link linkL"></div>
-          <div className="link linkM"></div>
-          <div className="link linkN"></div>
-          <div className="link linkO"></div>
-        </div>
+        <LinkGrid />
       </div>
     </main>
   )
