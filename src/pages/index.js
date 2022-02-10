79b32fa14a7437183d9b270c9aa0d1a18c730e80
Christina Giancola <christinamgiancola@gmail.com>
2022-02-09 20:37:20 -0500

    WIP mapping links to responsive bgImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,29 @@ export default function Home() {
   const [lightsOn, setLightsOn] = useState(true)
 
   return (
-    <main
-      className={lightsOn ? "bgImage lightsOn" : "bgImage lightsOff"}
-      onClick={() => setLightsOn(!lightsOn)}
-    ></main>
+    <main className="imageContainer">
+      <div
+        className={lightsOn ? "bgImage lightsOn" : "bgImage lightsOff"}
+      ></div>
+      <div className="linkContainer" onClick={() => setLightsOn(!lightsOn)}>
+        <div className="linkGrid">
+          <div className="link linkA"></div>
+          <div className="link linkB"></div>
+          <div className="link linkC"></div>
+          <div className="link linkD"></div>
+          <div className="link linkE"></div>
+          <div className="link linkF"></div>
+          <div className="link linkG"></div>
+          <div className="link linkH"></div>
+          <div className="link linkI"></div>
+          <div className="link linkJ"></div>
+          <div className="link linkK"></div>
+          <div className="link linkL"></div>
+          <div className="link linkM"></div>
+          <div className="link linkN"></div>
+          <div className="link linkO"></div>
+        </div>
+      </div>
+    </main>
   )
 }
