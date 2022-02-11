0de78749c40d7284b52aaf6bb3d577a54bb1ecff
Christina Giancola <christinamgiancola@gmail.com>
2022-02-11 17:34:51 -0500

    WIP toggle between lightsOn and lightsOff backgrounds

--- src/components/TruffleImageMap.js
+++ src/components/TruffleImageMap.js
@@ -1,183 +1,165 @@
 import React from "react"
 
-const TruffleImageMap = () => {
+const TruffleImageMap = props => {
   return (
     <map name="imgMap">
       <area
-        target=""
-        alt="btn11"
-        title="btn11"
-        href=""
-        coords="2012,1720,2235,1874"
-        shape="rect"
+        shape="poly"
+        title="screen"
+        alt="screen"
+        coords="1686,737,1744,737,2945,744,3017,759,3039,809,3039,874,3039,1560,3010,1604,2959,1618,1737,1618,1686,1604,1650,1560,1650,824,1665,766"
         onClick={e => {
-          alert("Button 11 Clicked")
+          alert("Screen Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn12"
-        title="btn12"
-        href=""
-        coords="2244,1720,2458,1874"
         shape="rect"
+        title="btn11"
+        alt="btn11"
+        coords="2012,1720,2235,1874"
+        onClick={e => {
+          console.log(props)
+          props.setLightsOn(true)
+          alert("Button 11 Clicked" + e.target.coords)
+          e.preventDefault()
+        }}
+      />
+      <area
+        shape="rect"
+        title="btn12"
+        alt="btn12"
+        coords="2244,1720,2458,1874"
         onClick={e => {
           alert("Button 12 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn13"
-        title="btn13"
-        href=""
-        coords="2463,1715,2708,1874"
         shape="rect"
+        title="btn13"
+        alt="btn13"
+        coords="2463,1715,2708,1874"
         onClick={e => {
           alert("Button 13 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn6"
-        title="btn6"
-        href=""
-        coords="1744,1865,2003,2011"
         shape="rect"
+        title="btn6"
+        alt="btn6"
+        coords="1744,1865,2003,2011"
         onClick={e => {
           alert("Button 6 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn7"
-        title="btn7"
-        href=""
-        coords="2003,1874,2235,2015"
         shape="rect"
+        title="btn7"
+        alt="btn7"
+        coords="2003,1874,2235,2015"
         onClick={e => {
           alert("Button 7 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn8"
-        title="btn8"
-        href=""
-        coords="2244,1879,2458,2011"
         shape="rect"
+        title="btn8"
+        alt="btn8"
+        coords="2244,1879,2458,2011"
         onClick={e => {
           alert("Button 8 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn9"
-        title="btn9"
-        href=""
-        coords="2463,1879,2708,2011"
         shape="rect"
+        title="btn9"
+        alt="btn9"
+        coords="2463,1879,2708,2011"
         onClick={e => {
           alert("Button 9 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn10"
-        title="btn10"
-        href=""
-        coords="2717,1870,2963,2015"
         shape="rect"
+        title="btn10"
+        alt="btn10"
+        coords="2717,1870,2963,2015"
         onClick={e => {
           alert("Button 10 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn1"
-        title="btn1"
-        href=""
-        coords="1730,2015,1990,2170"
         shape="rect"
+        title="btn1"
+        alt="btn1"
+        coords="1730,2015,1990,2170"
         onClick={e => {
           alert("Button 1 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn2"
-        title="btn2"
-        href=""
-        coords="1999,2020,2235,2179"
         shape="rect"
+        title="btn2"
+        alt="btn2"
+        coords="1999,2020,2235,2179"
         onClick={e => {
           alert("Button 2 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn3"
-        title="btn3"
-        href=""
-        coords="2244,2014,2453,2184"
         shape="rect"
+        title="btn3"
+        alt="btn3"
+        coords="2244,2014,2453,2184"
         onClick={e => {
           alert("Button 3 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn4"
-        title="btn4"
-        href=""
-        coords="2454,2014,2708,2184"
         shape="rect"
+        title="btn4"
+        alt="btn4"
+        coords="2454,2014,2708,2184"
         onClick={e => {
           alert("Button 4 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="btn5"
-        title="btn5"
-        href=""
-        coords="2717,2019,2990,2175"
         shape="rect"
+        title="btn5"
+        alt="btn5"
+        coords="2717,2019,2990,2175"
         onClick={e => {
           alert("Button 5 Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="infoBtn"
-        title="infoBtn"
-        href=""
-        coords="1762,1724,1999,1861"
         shape="rect"
+        title="infoBtn"
+        alt="infoBtn"
+        coords="1762,1724,1999,1861"
         onClick={e => {
           alert("Info Button Clicked")
           e.preventDefault()
         }}
       />
       <area
-        target=""
-        alt="contactBtn"
-        title="contactBtn"
-        href=""
-        coords="2713,1720,2963,1865"
         shape="rect"
+        title="contactBtn"
+        alt="contactBtn"
+        coords="2713,1720,2963,1865"
         onClick={e => {
           alert("Contact Button Clicked")
           e.preventDefault()
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import ImageMap from "image-map/dist/image-map"
 import TruffleImageMap from "../components/TruffleImageMap"
 import BackgroundLightsOnA from "../assets/images/BKG-A-landing.jpg"
 import BackgroundLightsOffA from "../assets/images/BKG-A-lightbox.jpg"
+import Card1 from "../assets/images/CARD01.jpg"
 import "normalize.css"
 import "../assets/main.css"
 
 export default function Home() {
-  const [lightsOn, setLightsOn] = useState(true)
+  const [lightsOn, setLightsOn] = useState(false)
 
   return (
     <>
@@ -20,38 +21,35 @@ export default function Home() {
       </Helmet>
       <main className="body">
         <div className="imageContainer">
-          <div className="cardStack">
-            <area
-              target=""
-              alt="screen"
-              title="screen"
-              href=""
-              coords="1686,737,1744,737,2945,744,3017,759,3039,809,3039,874,3039,1560,3010,1604,2959,1618,1737,1618,1686,1604,1650,1560,1650,824,1665,766"
-              shape="poly"
-              onClick={e => {
-                alert("Screen Clicked")
-                e.preventDefault()
+          <div className="cardStack"></div>
+          {lightsOn ? (
+            <img
+              src={BackgroundLightsOnA}
+              alt="Open Truffle Box on a Film Set"
+              height="3453"
+              width="5148"
+              useMap="#imgMap"
+              onLoad={() => {
+                ImageMap("img[usemap]")
               }}
             />
-            <div className="topCard"></div>
-            <div className="middleCard"></div>
-            <div className="bottomCard"></div>
-          </div>
-          {/* <img
-            src={BackgroundLightsOffA}
-            alt="Open Truffle Box on a Film Set with Lights Off"
-          /> */}
-          <img
-            src={BackgroundLightsOnA}
-            // height="3453"
-            // width="5148"
-            useMap="#imgMap"
-            alt="Open Truffle Box on a Film Set"
-            onLoad={() => {
-              ImageMap("img[usemap]")
-            }}
+          ) : (
+            <img
+              src={BackgroundLightsOffA}
+              alt="Open Truffle Box on a Film Set with Lights Off"
+              height="3453"
+              width="5148"
+              useMap="#imgMap"
+              onLoad={() => {
+                ImageMap("img[usemap]")
+              }}
+            />
+          )}
+          <TruffleImageMap
+            className="truffleImageMap"
+            lightsOn={lightsOn}
+            setLightsOn={setLightsOn}
           />
-          <TruffleImageMap />
         </div>
       </main>
     </>
