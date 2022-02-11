00c43ed77c8adc84761000fe7586e25e09f0ddfa
Christina Giancola <christinamgiancola@gmail.com>
2022-02-10 21:45:00 -0500

    WIP installed ImageMap plugin to make map area coords responsive

---
+++ src/html.js
@@ -0,0 +1,40 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+        <script src="https://unpkg.com/image-map/dist/image-map.js"></script>
+        <script src="https://unpkg.com/jquery/dist/jquery.js"></script>
+        <script src="https://unpkg.com/image-map/dist/image-map.jquery.js"></script>
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,64 @@
 import React, { useState } from "react"
-import LinkGrid from "../components/linkGrid"
+import { Helmet } from "react-helmet"
+import ImageMap from "image-map/dist/image-map"
+import BackgroundLightsOnA from "../assets/images/BKG-A-landing.jpg"
+// import BackgroundLightsOffB from "../assets/images/BKG-A-lightbox.jpg"
+// import LinkGrid from "../components/linkGrid"
 
 import "normalize.css"
-import "../assets/main.css"
+import "../assets/imageMap.css"
+// import "../assets/main.css"
 
 export default function Home() {
   const [lightsOn, setLightsOn] = useState(true)
 
   return (
-    <main className="imageContainer">
+    <>
+      <Helmet>
+        <script src="//code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+        <script src="https://unpkg.com/image-map/dist/image-map.js"></script>
+        <script src="https://unpkg.com/jquery/dist/jquery.js"></script>
+        <script src="https://unpkg.com/image-map/dist/image-map.jquery.js"></script>
+      </Helmet>
       <div
-        className={lightsOn ? "bgImage lightsOn" : "bgImage lightsOff"}
-      ></div>
-      <div className="linkContainer" onClick={() => setLightsOn(!lightsOn)}>
-        <LinkGrid />
+        className="imageContainer"
+        onLoad={() => {
+          console.log("imageContainer loaded")
+          ImageMap("img[usemap]")
+          console.log("ImageMap Done?")
+        }}
+      >
+        <img
+          src={BackgroundLightsOnA}
+          height="3453"
+          width="5148"
+          useMap="#imgMap"
+          alt="Open Truffle Box on a Film Set"
+          // className="backgroundImage"
+        />
+        <map name="imgMap">
+          <area
+            shape="rect"
+            coords="1742,1706,2019,1874"
+            href=""
+            alt="Info Button"
+            // onClick={() => setLightsOn(!lightsOn) }
+            onClick={e => {
+              alert("Info Button Clicked")
+              e.preventDefault()
+            }}
+          ></area>
+        </map>
       </div>
-    </main>
+
+      {/* <main className="imageContainer">
+        <div
+          className={lightsOn ? "bgImage lightsOn" : "bgImage lightsOff"}
+        ></div>
+        <div className="linkContainer" onClick={() => setLightsOn(!lightsOn)}>
+          <LinkGrid />
+        </div>
+      </main> */}
+    </>
   )
 }
