39135ecb1ca4fa4262e449b3c5890f930e1c247f
Christina Giancola <christinamgiancola@gmail.com>
2022-02-12 21:42:08 -0500

    Extracted screen dimensions from screenArea coords on window resize

--- src/components/TruffleImageMap.js
+++ src/components/TruffleImageMap.js
@@ -1,260 +1,265 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const TruffleImageMap = ({
   setLastClicked,
   setCurrentClick,
   setScreenCoords,
 }) => {
+  useEffect(() => {
+    // console.log("Truffle useEffect triggered")
+    let coords = document.getElementById("screenArea").coords
+    // console.log("coords updated: " + coords)
+    setScreenCoords(coords)
+  })
+
   return (
-    <map name="imgMap">
-      <area
-        id="videoPlayerArea"
-        shape="rect"
-        title="screen"
-        alt="screen"
-        coords="1714,796,2988,1556"
-        onLoad={e => {
-          console.log(e)
-          setScreenCoords(e.target.coords)
-        }}
-        onClick={e => {
-          setLastClicked("screen")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn11"
-        alt="btn11"
-        coords="2012,1720,2235,1874"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn12"
-        alt="btn12"
-        coords="2244,1720,2458,1874"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn13"
-        alt="btn13"
-        coords="2463,1715,2708,1874"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn6"
-        alt="btn6"
-        coords="1744,1865,2003,2011"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn7"
-        alt="btn7"
-        coords="2003,1874,2235,2015"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn8"
-        alt="btn8"
-        coords="2244,1879,2458,2011"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn9"
-        alt="btn9"
-        coords="2463,1879,2708,2011"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn10"
-        alt="btn10"
-        coords="2717,1870,2963,2015"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn1"
-        alt="btn1"
-        coords="1730,2015,1990,2170"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn2"
-        alt="btn2"
-        coords="1999,2020,2235,2179"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn3"
-        alt="btn3"
-        coords="2244,2014,2453,2184"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn4"
-        alt="btn4"
-        coords="2454,2014,2708,2184"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="btn5"
-        alt="btn5"
-        coords="2717,2019,2990,2175"
-        onClick={e => {
-          setLastClicked("truffle")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="infoBtn"
-        alt="infoBtn"
-        coords="1762,1724,1999,1861"
-        onClick={e => {
-          setLastClicked("info")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="contactBtn"
-        alt="contactBtn"
-        coords="2713,1720,2963,1865"
-        onClick={e => {
-          setLastClicked("contact")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="leftArrow"
-        alt="leftArrow"
-        coords="3115,1742,3311,1865"
-        onClick={e => {
-          setLastClicked("leftArrow")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="rightArrow"
-        alt="rightArrow"
-        coords="3115,1865,3324,2006"
-        onClick={e => {
-          setLastClicked("rightArrow")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="bgTop"
-        alt="bgTop"
-        coords="14,-5,5143,714"
-        onClick={e => {
-          setLastClicked("bg")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="bgLeft"
-        alt="bgLeft"
-        coords="9,719,1619,2288"
-        onClick={e => {
-          setLastClicked("bg")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="rect"
-        title="bgBottom"
-        alt="bgBottom"
-        coords="9,2297,5139,3453"
-        onClick={e => {
-          setLastClicked("bg")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-      <area
-        shape="poly"
-        title="bgRight"
-        alt="bgRight"
-        coords="3061,723,3061,1679,3352,1711,3361,1852,3379,2015,3247,2065,3115,2088,3133,2293,3333,2288,3634,2284,5143,2284,5134,719"
-        onClick={e => {
-          setLastClicked("bg")
-          setCurrentClick(e.target.alt)
-          e.preventDefault()
-        }}
-      />
-    </map>
+    <div>
+      <map name="imgMap">
+        <area
+          id="screenArea"
+          shape="rect"
+          title="screen"
+          alt="screen"
+          coords="1714,796,2988,1556"
+          onClick={e => {
+            setLastClicked("screen")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn11"
+          alt="btn11"
+          coords="2012,1720,2235,1874"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn12"
+          alt="btn12"
+          coords="2244,1720,2458,1874"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn13"
+          alt="btn13"
+          coords="2463,1715,2708,1874"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn6"
+          alt="btn6"
+          coords="1744,1865,2003,2011"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn7"
+          alt="btn7"
+          coords="2003,1874,2235,2015"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn8"
+          alt="btn8"
+          coords="2244,1879,2458,2011"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn9"
+          alt="btn9"
+          coords="2463,1879,2708,2011"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn10"
+          alt="btn10"
+          coords="2717,1870,2963,2015"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn1"
+          alt="btn1"
+          coords="1730,2015,1990,2170"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn2"
+          alt="btn2"
+          coords="1999,2020,2235,2179"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn3"
+          alt="btn3"
+          coords="2244,2014,2453,2184"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn4"
+          alt="btn4"
+          coords="2454,2014,2708,2184"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="btn5"
+          alt="btn5"
+          coords="2717,2019,2990,2175"
+          onClick={e => {
+            setLastClicked("truffle")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="infoBtn"
+          alt="infoBtn"
+          coords="1762,1724,1999,1861"
+          onClick={e => {
+            setLastClicked("info")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="contactBtn"
+          alt="contactBtn"
+          coords="2713,1720,2963,1865"
+          onClick={e => {
+            setLastClicked("contact")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="leftArrow"
+          alt="leftArrow"
+          coords="3115,1742,3311,1865"
+          onClick={e => {
+            setLastClicked("leftArrow")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="rightArrow"
+          alt="rightArrow"
+          coords="3115,1865,3324,2006"
+          onClick={e => {
+            setLastClicked("rightArrow")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="bgTop"
+          alt="bgTop"
+          coords="14,-5,5143,714"
+          onClick={e => {
+            setLastClicked("bg")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="bgLeft"
+          alt="bgLeft"
+          coords="9,719,1619,2288"
+          onClick={e => {
+            setLastClicked("bg")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="rect"
+          title="bgBottom"
+          alt="bgBottom"
+          coords="9,2297,5139,3453"
+          onClick={e => {
+            setLastClicked("bg")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+        <area
+          shape="poly"
+          title="bgRight"
+          alt="bgRight"
+          coords="3061,723,3061,1679,3352,1711,3361,1852,3379,2015,3247,2065,3115,2088,3133,2293,3333,2288,3634,2284,5143,2284,5134,719"
+          onClick={e => {
+            setLastClicked("bg")
+            setCurrentClick(e.target.alt)
+            e.preventDefault()
+          }}
+        />
+      </map>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,60 @@
 import React, { useState, useEffect } from "react"
 import { Helmet } from "react-helmet"
+import { useResizeObserver } from "@mantine/hooks"
 import ImageMap from "image-map/dist/image-map"
 import TruffleImageMap from "../components/TruffleImageMap"
 import BackgroundLightsOnA from "../assets/images/BKG-A-landing.jpg"
 import BackgroundLightsOffA from "../assets/images/BKG-A-lightbox.jpg"
-import Card1 from "../assets/images/CARD01.jpg"
+
 import "normalize.css"
 import "../assets/main.css"
+import reactDom from "react-dom"
 
 export default function Home() {
   const [lightsOn, setLightsOn] = useState(true)
   const [lastClicked, setLastClicked] = useState("")
   const [currentClick, setCurrentClick] = useState("")
-  const [screenCoords, setScreenCoords] = useState("")
+  const [screenCoords, setScreenCoords] = useState()
+  // const [currentWidth, setCurrentWidth] = useState("")
+  // const [currentHeight, setCurrentHeight] = useState("")
 
-  const getScreenCoords = () => {
-    // console.log("getting screen coords: " + screenCoords)
-    setScreenCoords(document.getElementById("videoPlayerArea").coords)
-    // console.log("set screen coords: " + screenCoords)
+  const [ref, rect] = useResizeObserver()
+
+  let cardTest = document.getElementById("cardTest")
+
+  let cardWidth = 0
+  let cardHeight = 0
+
+  //dimensions measured in pixels
+  const convertCoordsToDimensions = () => {
+    // console.log("convertCoordsToDimensions triggered")
+    if (screenCoords) {
+      let coordArray = screenCoords.split(",")
+      cardWidth = Math.floor(coordArray[2]) - Math.floor(coordArray[0])
+      cardHeight = Math.floor(coordArray[3]) - Math.floor(coordArray[1])
+      // console.log("new width: " + cardWidth + " and height: " + cardHeight)
+      document.getElementById("cardTest").setAttribute("height", cardHeight)
+      document.getElementById("cardTest").setAttribute("width", cardWidth)
+    }
   }
+
+  // console.log("About to Get Dimensions")
+  // convertCoordsToDimensions()
 
   useEffect(() => {
-    // window.addEventListener("resize", getScreenCoords)
-    getScreenCoords()
-    window.removeEventListener("resize", getScreenCoords)
-  }, [])
-
-  const handleClick = () => {
-    toggleLights()
-  }
+    // console.log("useEffect(Home) triggered")
+    convertCoordsToDimensions()
+  })
 
   const toggleLights = () => {
     if (!lightsOn && (lastClicked === "contact" || lastClicked === "info")) {
+      // console.log("toggleLights triggered: A")
       setLightsOn(true)
     } else if (lightsOn && lastClicked === "truffle") {
+      // console.log("toggleLights triggered: B")
       setLightsOn(false)
     } else if (!lightsOn && lastClicked === "bg") {
+      // console.log("toggleLights triggered: C")
       setLightsOn(true)
     }
   }
@@ -49,43 +68,46 @@ export default function Home() {
         <script src="https://unpkg.com/image-map/dist/image-map.jquery.js"></script>
       </Helmet>
       <main className="body">
-        <div className="imageContainer">
-          <div className="cardStack"></div>
-          {lightsOn ? (
-            <img
-              src={BackgroundLightsOnA}
-              alt="Open Truffle Box on a Film Set"
-              className="fadeIn"
-              height="3453"
-              width="5148"
-              useMap="#imgMap"
-              onresize={() => {
-                console.log(document.getElementById("videoPlayerArea").coords)
-              }}
-              onLoad={() => {
-                ImageMap("img[usemap]")
-                console.log(document.getElementById("videoPlayerArea").coords)
-              }}
-            />
-          ) : (
-            <img
-              src={BackgroundLightsOffA}
-              alt="Open Truffle Box on a Film Set with Lights Off"
-              height="3453"
-              width="5148"
-              useMap="#imgMap"
-              onLoad={() => {
-                ImageMap("img[usemap]")
-                console.log(document.getElementById("videoPlayerArea").coords)
-              }}
-            />
-          )}
+        <div>
+          <div ref={ref} className="imageContainer">
+            {lightsOn ? (
+              <img
+                src={BackgroundLightsOnA}
+                alt="Open Truffle Box on a Film Set"
+                className="fadeIn"
+                height="3453"
+                width="5148"
+                useMap="#imgMap"
+                onLoad={() => {
+                  ImageMap("img[usemap]")
+                  // console.log("lights on img loaded")
+                  convertCoordsToDimensions()
+                }}
+              />
+            ) : (
+              <img
+                src={BackgroundLightsOffA}
+                alt="Open Truffle Box on a Film Set with Lights Off"
+                height="3453"
+                width="5148"
+                useMap="#imgMap"
+                onLoad={() => {
+                  ImageMap("img[usemap]")
+                  // console.log("lights off img loaded")
+                  convertCoordsToDimensions()
+                }}
+              />
+            )}
+            <div id="cardTest"></div>
+          </div>
           <TruffleImageMap
-            className="truffleImageMap"
+            id="truffleImageMap"
             setLastClicked={setLastClicked}
             setCurrentClick={setCurrentClick}
             setScreenCoords={setScreenCoords}
-            onClick={handleClick()}
+            cardHeight={cardHeight}
+            cardWidth={cardWidth}
+            onClick={toggleLights()}
           />
         </div>
       </main>
