2915ec8701724524d1da48ea066f25d44f53ac55
cjmaret <cjmaret@gmail.com>
2022-04-03 00:06:01 +0700

    feat: GTM config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,43 @@ require('dotenv').config({
   path: `.env`,
 });
 
+/**
+ * The currently active environment.
+ * This is used to set the corresponding Tag Manager environment config.
+ */
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
+console.log(`Using environment config: '${activeEnv}'`);
+
+// The Tag Manager Container ID.
+const gtmContainerId = 'GTM-MC7WV3M';
+
+/**
+ * Tag Manager Environment values to configure gatsby-plugin-google-tagmanager.
+ * null values will cause the default (live/production) snippet to load.
+ */
+const gtmEnv = {
+  // If tag manager plugin is configured with includeInDevelopment set to
+  // true then you should create a corresponding Development environment in
+  // Tag Manager and replace the null values with the container environment
+  // auth and preview values. Otherwise the production snippet will load.
+  development: {
+    gtmAuth: null,
+    gtmPreview: null,
+  },
+
+  staging: {
+    gtmAuth: 'Cg319kjsWc6vaqZmLPIw3w',
+    gtmPreview: 'env-3',
+  },
+
+  // According to GTM docs you should use standard tag for prod so we'll set to null.
+  production: {
+    gtmAuth: null,
+    gtmPreview: null,
+  },
+};
+
 // .${process.env.NODE_ENV}
 
 module.exports = {
@@ -61,64 +98,12 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        id: 'YOUR_GOOGLE_TAGMANAGER_ID',
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
+        id: gtmContainerId,
         includeInDevelopment: false,
 
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: 'gatsby' },
-
-        // Specify optional GTM environment details.
-        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-        dataLayerName: 'YOUR_DATA_LAYER_NAME',
-
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME',
-        // Defaults to false
-        enableWebVitalsTracking: true,
-        // Defaults to https://www.googletagmanager.com
-        selfHostedOrigin: 'YOUR_SELF_HOSTED_ORIGIN',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'example.com',
-        // defaults to false
-        enableWebVitalsTracking: true,
+        // GTM environment details.
+        gtmAuth: gtmEnv[activeEnv].gtmAuth,
+        gtmPreview: gtmEnv[activeEnv].gtmPreview,
       },
     },
   ],
