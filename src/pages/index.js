c654a5ae7a6470e809977ec7845e8882d34f2a62
cjmaret <cjmaret@gmail.com>
2022-04-09 16:13:18 +0700

    feat: title and favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,7 @@ export default function Home() {
         <script src="https://unpkg.com/image-map/dist/image-map.js"></script>
         <script src="https://unpkg.com/jquery/dist/jquery.js"></script>
         <script src="https://unpkg.com/image-map/dist/image-map.jquery.js"></script>
-        {/* <script src="web-animations.min.js"></script> */}
-        {/* <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        /> */}
+<title>The Confectionery</title>
       </Helmet>
       <main className="main">
         <App arrowClickedStack={arrowClickedStack} />
